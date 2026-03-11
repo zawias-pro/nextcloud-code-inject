69c5ac311e409b563a2d33defd7ab42560754a16
Paweł Zawiślak <zawias-pro@users.noreply.github.com>
2026-03-11 18:37:37 +0100

    remove unused imports

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -4,15 +4,11 @@ declare(strict_types=1);
 
 namespace OCA\Codeinjector\AppInfo;
 
-use OCA\Codeinjector\Listener\TemplateListener;
 use OCA\Codeinjector\Middleware\InjectMiddleware;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
-use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
-use OCP\EventDispatcher\Event;
-use OCP\EventDispatcher\IEventDispatcher;
 
 class Application extends App implements IBootstrap {
 
